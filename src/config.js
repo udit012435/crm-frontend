f0632dd2402b4296a6db290da862e4947aa64f32
udit012435 <udit012435@gmail.com>
2025-09-17 18:35:19 +0530

    env update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "DemoCRM";
 
-export const BASE_URL = import.meta.env.example.VITE_API_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_VERSION = "v1";
